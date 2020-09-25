819eeb84b88e919c9cb5d75d0d1a1040739cc709
psreenagakri <psreenagakri@IN-5CG0251NJV>
2020-09-25 04:06:51 -0700

    my application

---
+++ MyApplication/src/com/dxc/helloworld.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+		System.out.println("this is java project");
+
+	}
+
+}
